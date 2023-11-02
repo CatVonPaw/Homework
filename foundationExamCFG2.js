f11387911e734a7433b6b3be7173051247165ae4
Katalin <145605450+CatVonPaw@users.noreply.github.com>
2023-11-02 20:26:03 +0000

    Add files via upload

---
+++ foundationExamCFG2.js
@@ -0,0 +1,11 @@
+<script>
+  function addTask(){
+    const addTask  = document.getElementById(addTask);
+
+    const listOfTask = document.createElement("listOfTask");
+    const taskType = document.createElement("taskType");
+    <input type = "checkbox" id="myCheck">
+      
+    </input>
+  }
+</script>
